d6f7c8457448d318fb16944ae4362621b66680d0
Herik Colares Rezende <dev.herik@gmail.com>
2025-07-12 14:30:18 -0300

    feat: add LoadingCircleSpinner component with motion animation and styles

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -10,7 +10,7 @@ export default function Button({
   return (
     <button
       onClick={onClick}
-      className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition cursor-pointer"
+      className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition cursor-pointer w-full disabled:opacity-50 disabled:cursor-not-allowed"
     >
       {children}
     </button>
