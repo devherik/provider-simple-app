f70176049807d9b72bac40a1205be8a02cb3a227
Herik Colares Rezende <dev.herik@gmail.com>
2025-07-12 14:13:28 -0300

    fix: update input class for consistent styling in Form component

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -3,7 +3,7 @@ export default function Form({
   value,
   id,
   type,
-  label
+  label,
 }: {
   onChange: (value: string) => void;
   value: string;
@@ -24,7 +24,7 @@ export default function Form({
         id={id}
         value={value}
         onChange={(e) => onChange(e.target.value)}
-        className="w-full px-3 py-2 border border-gray-300 text-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="w-full px-3 py-2 border border-gray-300 text-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-[#020202] focus:border-transparent"
         placeholder={`Enter any ${label.toLowerCase()}`}
       />
     </>
