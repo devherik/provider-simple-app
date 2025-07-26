558e7d01575fe78af7d709e4feff9411a4ce7e45
Herik Colares Rezende <dev.herik@gmail.com>
2025-07-26 05:54:47 -0300

    feat: refactor ThemeToggle component to use CSS module for styling and simplify structure

--- src/components/theme_toogle/ThemeToogle.tsx
+++ src/components/theme_toogle/ThemeToogle.tsx
@@ -1,18 +1,16 @@
 // In any component:
 import { useTheme } from "../../hooks/useTheme";
+import style from "./toogle.module.css";
 
 export default function ThemeToggle() {
   const { theme, setTheme, currentTheme } = useTheme();
-  
+
   return (
-    <div style={{
-      backgroundColor: currentTheme.colors.background,
-      color: currentTheme.colors.text,
-      fontSize: currentTheme.fontSizes.medium
-    }}>
-      <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
-        Toggle Theme
-      </button>
-    </div>
+    <button
+      className={style.button}
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+    >
+      Toggle Theme
+    </button>
   );
 }
