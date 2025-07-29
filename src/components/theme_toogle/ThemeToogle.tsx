31dc918fb570e116fa49ed93429d56e3f980594d
Herik Colares Rezende <dev.herik@gmail.com>
2025-07-29 10:46:26 -0300

    refactor: update ThemeToggle component styles and remove unused icons

--- src/components/theme_toogle/ThemeToogle.tsx
+++ src/components/theme_toogle/ThemeToogle.tsx
@@ -2,8 +2,6 @@
 
 import { useTheme } from "../../hooks/useTheme";
 import style from "./toogle.module.css";
-import day_icon from "./sun.svg";
-import night_icon from "./moon.svg";
 
 export default function ThemeToggle() {
   const { theme, setTheme } = useTheme();
@@ -14,10 +12,6 @@ export default function ThemeToggle() {
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
     >
       <div className={style.body}>
-        <div className={style.icons}>
-          <img src={day_icon} alt="Day Icon" />
-          <img src={night_icon} alt="Night Icon" />
-        </div>
         <span
           className={`${style.toggle} ${theme === "dark" ? style.active : ""}`}
         />
